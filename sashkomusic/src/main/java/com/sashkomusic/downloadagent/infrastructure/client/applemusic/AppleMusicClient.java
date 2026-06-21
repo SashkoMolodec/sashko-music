de5046523838aa9bdcaf5029e4b8b6ce4eac724c
Sasha <sanykk2014@gmail.com>
2026-06-21 13:28:11 +0300

    dependencies fix

--- sashkomusic/src/main/java/com/sashkomusic/downloadagent/infrastructure/client/applemusic/AppleMusicClient.java
+++ sashkomusic/src/main/java/com/sashkomusic/downloadagent/infrastructure/client/applemusic/AppleMusicClient.java
@@ -106,7 +106,7 @@ public class AppleMusicClient implements MusicSourcePort {
                     "--cookies-path", cookiesPath,
                     "--output-path", outputPath,
                     "--language", "uk",
-                    "--codec-song", "aac-legacy"
+                    "--song-codec", "aac-legacy"
             );
 
             activeProcesses.put(releaseId, process);
